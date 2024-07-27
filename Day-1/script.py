76cea22c769df05805df6df334a4a8e8b6651c57
Fanoos Haider <164775827+FanoosHaider2024@users.noreply.github.com>
2024-07-27 11:29:03 +0500

    Add files via upload

---
+++ Day-1/script.py
@@ -0,0 +1,12 @@
+'''
+# Top 10 extensions
+1. python
+2. Intellicode
+3. Jupyter
+4. Jupyter keymap
+5. CodeSnap   #  cntrl + shift + X (for finding detail of extension)
+6. Markdown All in One
+7. prettier
+8. Kite
+9. Blackbox
+'''
